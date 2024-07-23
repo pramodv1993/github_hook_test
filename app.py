8318dd81e87d2ce60b4ff521693d5a720ff9cf5b
Pramod Vadiraja <pramodv1993@gmail.com>
2024-07-23 15:46:23 +0200

    debug

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from typing import Dict
 
 app = FastAPI()
@@ -9,8 +9,16 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/postreceive/")
-async def display_msg(data: Dict):
-    received_data = {"message_received": data}
+async def display_msg(req: Request):
+    received_data = await req.body()
+    received_data = received_data.decode('utf-8')
     print(received_data)
+    # if req.headers['Content-Type'] == 'application/json':
+    #     item = await req.json()
+    # elif req.headers['Content-Type'] == 'multipart/form-data':
+    #     item = await req.form()
+    # elif req.headers['Content-Type'] == 'application/x-www-form-urlencoded':
+    #     item = await req.form()
+    # received_data = {"message_received": item}
     return received_data
 
