05ce28c257199b89c7fd2e3468279abf3f76c489
Pramod Vadiraja <pramodv1993@gmail.com>
2024-07-23 15:58:18 +0200

    debug

--- app.py
+++ app.py
@@ -11,14 +11,12 @@ async def root():
 @app.post("/postreceive/")
 async def display_msg(req: Request):
     received_data = await req.body()
-    received_data = received_data.decode('utf-8')
-    print(received_data)
-    # if req.headers['Content-Type'] == 'application/json':
-    #     item = await req.json()
-    # elif req.headers['Content-Type'] == 'multipart/form-data':
-    #     item = await req.form()
-    # elif req.headers['Content-Type'] == 'application/x-www-form-urlencoded':
-    #     item = await req.form()
-    # received_data = {"message_received": item}
+    if req.headers['Content-Type'] == 'application/json':
+        item = await req.json()
+    elif req.headers['Content-Type'] == 'multipart/form-data':
+        item = await req.form()
+    elif req.headers['Content-Type'] == 'application/x-www-form-urlencoded':
+        item = await req.form()
+    received_data = {"message_received": item}
     return received_data
 
